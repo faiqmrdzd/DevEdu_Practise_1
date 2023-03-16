32ca33d57c79bfc6e1a4ff74c0e296690e8537f3
Edvard909 <edi-9@inbox.ru>
2023-03-16 15:56:11 +0400

    Add project files.

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = new string[4]; 
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                string newInput = Console.ReadLine();
+
+                inputs[i] = newInput;
+            }
+
+            string[,] outputs = new string[4, 4] 
+            
+                for (int i = 0; i < outputs.Length; i++) 
+            {
+                outputs[0, i] = "a";
+            }
+            
+        }
+    }
+}
